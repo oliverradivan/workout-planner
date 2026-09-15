3f2f9e29805e40939f905b994f1b16e3b77f891c
oliver <oliverradivan@gmail.com>
2026-09-15 15:32:09 +0300

    final

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: /^react-native$/, replacement: 'react-native-web' }
-    ]
-  }
+    alias: {
+      'react-native': 'react-native-web',
+    },
+    extensions: [
+      '.web.tsx',
+      '.web.ts',
+      '.web.jsx',
+      '.web.js',
+      '.tsx',
+      '.ts',
+      '.jsx',
+      '.js',
+    ],
+  },
+  define: {
+    global: 'window',
+  },
 })
