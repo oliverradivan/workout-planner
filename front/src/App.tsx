a2b5685855cbb28a7791b88fa914fccef5a796f0
oliver <oliverradivan@gmail.com>
2026-09-15 15:45:20 +0300

    css

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StyleSheet, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
@@ -26,17 +27,27 @@ const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Questionnaire">
-        <Stack.Screen name="Questionnaire" component={QuestionnaireScreen} />
-        <Stack.Screen name="Preview" component={WorkoutPreviewScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Workout" component={WorkoutScreen} />
-        <Stack.Screen name="Week" component={WorkoutWeekScreen} />
-        <Stack.Screen name="Privacy" component={PrivacyPolicyScreen} />
-        <Stack.Screen name="Terms" component={TermsOfServiceScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Questionnaire">
+          <Stack.Screen name="Questionnaire" component={QuestionnaireScreen} />
+          <Stack.Screen name="Preview" component={WorkoutPreviewScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Workout" component={WorkoutScreen} />
+          <Stack.Screen name="Week" component={WorkoutWeekScreen} />
+          <Stack.Screen name="Privacy" component={PrivacyPolicyScreen} />
+          <Stack.Screen name="Terms" component={TermsOfServiceScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+  },
+})
